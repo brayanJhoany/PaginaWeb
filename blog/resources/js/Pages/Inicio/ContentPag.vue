77c63c4509016b56923db5b2e5c55f1433b17d5c
Brayan Escobar <brayan.escobar@live.com>
2020-11-28 15:45:31 -0300

    Home

---
+++ blog/resources/js/Pages/Inicio/ContentPag.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-container fluid class="mt-2">
+        <b-row>
+            <b-col cols="8" class="mt-4">
+
+
+            </b-col>
+            <b-col cols="4" class="text-center">
+                hola!!
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+    name: "ContentPag",
+    data() {
+        return {
+
+        }
+    },
+};
+</script>
---
+++ blog/resources/js/Pages/Inicio/HeaderPag.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <b-navbar  toggleable="md" type="dark" variant="danger">
+            <b-container >
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                <b-navbar-brand class="mr-4" href="#">{{ titulo }}</b-navbar-brand>
+
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav>
+                        <b-nav-item href="#">Blog</b-nav-item>
+                        <b-nav-item href="#">Contacto</b-nav-item>
+                    </b-navbar-nav>
+                    <b-navbar-nav class="ml-auto">
+                        <b-button variant="success">Iniciar sesion</b-button>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-container>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderPag",
+    data() {
+        return {
+            titulo: "Pagina Web"
+        };
+    }
+};
+</script>
